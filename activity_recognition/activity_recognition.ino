85b453120f32a4b7590998e6debf28b42422f0e2
rokcej <rokcej1997@gmail.com>
2021-06-05 02:07:28 +0200

    First commit

---
+++ activity_recognition/activity_recognition.ino
@@ -0,0 +1,147 @@
+#include <Wire.h>
+#include <Ticker.h>
+
+// WiFi
+#define NETWORK_SSID "Zoxx"
+#define NETWORK_PASSWORD "09889576"
+#define NETWORK_MODE 1 // Access point = 0, Station = 1
+
+// MPU-9250
+#define I2C_ADD_MPU 0x68 // MPU-9250 I2C Address
+#define ACCEL_XOUT_H 0x3B
+#define GYRO_XOUT_H 0x43
+
+// Constants
+const float G = 9.80665f;
+
+const int FREQUENCY = 100;
+const float TIME_CONSTANT = 1.f;
+
+const float DT = 1.f / FREQUENCY;
+const float ALPHA = TIME_CONSTANT / (TIME_CONSTANT + DT);
+
+
+// Global variables
+Ticker ticker;
+float rot[2] = { 0.f };
+float acc_err[3] = { 0.f };
+float gyro_err[3] = { 0.f };
+
+
+
+// Function declarations
+void setupServer();
+void loopServer();
+
+float rad2deg(float angle) {
+    return angle * 180.f / M_PI;                                
+}
+
+void calibrate() {
+  for (int i = 0; i < 3; ++i) {
+    acc_err[i] = 0.f;
+    gyro_err[i] = 0.f;
+  }
+
+  Serial.print("Calibrating, please don't move the board for a few seconds... ");
+  int samples = 300;
+  int frequency = 100;
+  for (int i = 0; i < samples; ++i) {
+    float acc[3], ang_vel[3];
+    readAcc(acc);
+    readGyro(ang_vel);
+
+    for (int i = 0; i < 3; ++i) {
+      acc_err[i] -= acc[i];
+      gyro_err[i] -= ang_vel[i];
+    }
+
+    delay(1000 / frequency);
+  }
+  Serial.println("Done!");
+
+  for (int i = 0; i < 3; ++i) {
+    acc_err[i] /= (float)samples;
+    gyro_err[i] /= (float)samples;
+  }
+
+  acc_err[2] += G;
+}
+
+void readAcc(float acc[3]) {
+  Wire.beginTransmission(I2C_ADD_MPU);
+  Wire.write(ACCEL_XOUT_H);
+  Wire.endTransmission();
+
+  Wire.requestFrom(I2C_ADD_MPU, 6);
+  for (int i = 0; i < 3; ++i) {
+    int16_t acc_out = (Wire.read() << 8) | Wire.read();
+    acc[i] = (float)acc_out * (2.f * G) / (1 << 15);
+  }
+}
+
+void readGyro(float ang_vel[3]) {
+  Wire.beginTransmission(I2C_ADD_MPU);
+  Wire.write(GYRO_XOUT_H);
+  Wire.endTransmission();
+
+  Wire.requestFrom(I2C_ADD_MPU, 6);
+  for (int i = 0; i < 3; ++i) {
+    int16_t ang_vel_out = (Wire.read() << 8) | Wire.read();
+    ang_vel[i] = (float)ang_vel_out / 131.f; // GYRO_SENSITIVITY = 131 s/deg = 
+  }
+}
+
+void updateAcc() {
+  float acc[3], ang_vel[3];
+  readAcc(acc);
+  readGyro(ang_vel);
+  for (int i = 0; i < 3; ++i) {
+    acc[i] += acc_err[i];
+    ang_vel[i] += gyro_err[i];
+  }
+
+  float rot_acc[2] = {
+    rad2deg(atan2f(acc[1], acc[2])), // Roll
+    rad2deg(atan2f(-acc[0], sqrtf(acc[1] * acc[1] + acc[2] * acc[2]))) // Pitch
+  };
+
+
+  for (int i = 0; i < 2; ++i)
+    rot[i] = ALPHA * (rot[i] + ang_vel[i] * DT) + (1.f - ALPHA) * (rot_acc[i]);
+  
+//  Serial.print(acc[0]);
+//  Serial.print(", ");
+//  Serial.print(acc[1]);
+//  Serial.print(", ");
+//  Serial.print(acc[2]);
+//  Serial.println();
+
+//  Serial.print("Roll: ");
+//  Serial.print(rot[0]);
+//  Serial.print(", Pitch: ");
+//  Serial.println(rot[1]);
+}
+
+void setup() {
+  // Initialize serial communication
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(100);
+  }
+  delay(500);
+
+  // Initialize I2C
+  Wire.begin(12, 14); // SDA = 12, SCL = 14
+  Wire.setClock(100000); // 100kHz
+
+  // Init server
+  setupServer();
+
+  calibrate();
+  ticker.attach_ms(1000 / FREQUENCY, updateAcc);
+}
+
+void loop() {
+  loopServer();
+}
---
+++ activity_recognition/server.ino
@@ -0,0 +1,93 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+ESP8266WebServer server(80);
+
+// Construct HTML string
+String getHTML(String head, String body) {
+    String style = "body, html { font-family: sans-serif; }";
+    return "<!DOCTYPE html><html>"
+        "<head>" + head + "<meta charset=\"UTF-8\"><style>" + style + "</style></head>"
+        "<body>" + body + "</body>"
+        "</html>";
+}
+
+// Root page can be accessed only if authentication is ok
+void handleRoot() {
+    int seconds = millis() / 1000;
+    int minutes = seconds / 60;
+    int hours = minutes / 60;
+    minutes %= 60;
+    seconds %= 60;
+    
+    char uptimeCStr[64];
+    snprintf(uptimeCStr, sizeof(uptimeCStr), "%02d:%02d:%02d", hours, minutes, seconds);
+    String uptimeStr = String(uptimeCStr);
+
+    String userAgent = server.header("User-Agent");
+    
+    String body = "<h2>Welcome to ESP8266</h2>"
+        "<p>Uptime: " + uptimeStr + "</p>"
+        "<p>User agent: " + userAgent + "</p>";
+    String html = getHTML("<title>Home</title>", body);
+    server.send(200, "text/html", html);
+}
+
+void handleData() {
+    String json = "{ \"rot\": [" + String(rot[0]) + ", " + String(rot[1]) + "] }";
+    server.send(200, "application/json", json);
+}
+
+void handleNotFound() {
+    String body ="<h2>404 - Page not found</h2>"
+        "<p>Click <a href=\"/\">here</a> to go to the main page</p>";
+    String html = getHTML("<title>Page not found</title>", body);
+    server.send(404, "text/html", html);
+}
+
+void setupWiFiAP() {
+    Serial.print("Setting up access point... ");
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(NETWORK_SSID, NETWORK_PASSWORD);
+    Serial.println("Done!");
+    Serial.print("Local IP: ");
+    Serial.println(WiFi.softAPIP());
+}
+
+void setupWiFiSTA() {
+    Serial.print("Connecting to access point... ");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(NETWORK_SSID, NETWORK_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+    }
+    Serial.println("Connected!");
+    Serial.print("Local IP: ");
+    Serial.println(WiFi.localIP());
+}
+
+void setupServer() {
+    
+#if NETWORK_MODE == 0
+    setupWiFiAP();
+#else if NETWORK_MODE == 1
+    setupWiFiSTA();
+#endif
+
+    server.on("/", handleRoot);
+    server.on("/data", handleData);
+    server.onNotFound(handleNotFound);
+
+    // Headers to track
+    const char * headerkeys[] = { "User-Agent", "Cookie" } ;
+    size_t headerkeyssize = sizeof(headerkeys) / sizeof(char*);
+    server.collectHeaders(headerkeys, headerkeyssize);
+
+    // Start the server
+    server.begin();
+    Serial.println("HTTP server started");
+}
+
+void loopServer() {
+    server.handleClient();
+}
